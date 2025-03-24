a1336086bb138c35cc140f4bd0ca8d82ada6200e
Carlos Arturo Baron Estrada <158487599+Carlos1AB1@users.noreply.github.com>
2025-03-23 20:29:32 -0500

    Update CorsConfig.java

--- demo/src/main/java/com/example/demo/config/CorsConfig.java
+++ demo/src/main/java/com/example/demo/config/CorsConfig.java
@@ -7,16 +7,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
+
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
+                        // Cambia la URL al dominio que necesites permitir.
                         .allowedOrigins("https://carlos1ab1.github.io")
-                        .allowedMethods("*")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        // Otras opciones posibles: "http://localhost:3000", etc.
+                        .allowedMethods("*")     // GET, POST, PUT, DELETE, etc.
+                        .allowedHeaders("*")     // Permite cualquier header
+                        .allowCredentials(true); // Permite el uso de cookies o credenciales
             }
         };
     }
